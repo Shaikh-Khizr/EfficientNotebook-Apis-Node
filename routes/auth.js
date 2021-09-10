bdcbfb8214cbac9ed22dcf7514fd6fbf9e73904a
Khizr Abdul Ahad Shaikh <khizars949@gmail.com>
2021-09-10 08:40:47 +0530

    Video 70 of course and 29 of project completed.

--- routes/auth.js
+++ routes/auth.js
@@ -14,16 +14,17 @@ router.post('/createuser', [
     body('email', 'Enter a valid email').isEmail(),
     body('password', 'Password must be at least 8 characters').isLength({ min: 8 }),
 ], async (req, res) => {
+    let success = false;
     // If there are errors, return Bad request and the errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
+        return res.status(400).json({ success, errors: errors.array() });
     }
     try {
         // Check weather the user with this emial exists already
         let user = await User.findOne({ email: req.body.email });
         if (user) {
-            return res.status(400).json({ error: "Sorry a user with this email already exists" })
+            return res.status(400).json({ success, error: "Sorry a user with this email already exists" })
         }
         // Secure the password
         const salt = await bcrypt.genSalt(10);
@@ -44,11 +45,12 @@ router.post('/createuser', [
         const authToken = jwt.sign(data, JWT_SECRET);
 
         // res.json(user);
-        res.json({ authToken });
+        success = true;
+        res.json({ success, authToken });
 
     } catch (error) {
         console.error(error.message);
-        res.status(500).send("Internal Server Error");
+        res.status(500).send(success, "Internal Server Error");
     }
 });
 
